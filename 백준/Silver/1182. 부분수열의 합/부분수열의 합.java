dd83a7b4b12b4e8e829fc0238336b4acb888cd12
kkoldduck2 <vkqmffjtm@naver.com>
2022-08-01 14:52:34 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,43 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * N개의 정수로 이뤄진 수열
+ * 크기가 양수인 부분 수열 중에서 그 수열의 원소를 다 더한 값이 S가 되는 경우의 수를 구하라
+ *
+ */
 public class Main {
-	static int ans;
 	static int n;
 	static int s;
 	static int[] arr;
+	static int result=0;
 	
-	public static void backTracking(int depth, int sum) {
-		
-		if(depth == n) {
+	static void dfs(int rst, int k, int cnt) {
+		if(k==n) {
+			if(rst==s && cnt>0) {
+				result++;
+			}
 			return;
 		}
 		
-		if(sum+arr[depth] == s) {
-//			System.out.println("depth: "+depth+": chosen = "+chosen);
-			ans ++;
-//			return;
-		}
-		
-		// depth를 포함함
-		backTracking(depth+1, sum+arr[depth] );
-		
-		// depth를 포함하지 않음
-		backTracking(depth+1, sum);
-		
+		dfs(rst+arr[k], k+1, cnt+1);	// arr[k]를 부분수열의 원소로 선택함
+		dfs(rst, k+1, cnt);	// arr[k]를 부분수열의 원소로 선택 안함
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		s = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
 		arr = new int[n];
-		ans = 0;
+		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		backTracking(0, 0);
+		dfs(0, 0, 0);
 		
-		System.out.println(ans);
+		System.out.println(result);
 	}
 }
